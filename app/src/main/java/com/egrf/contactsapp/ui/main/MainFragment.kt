28a8c4791b961fe246d4a9816beb0dc7e5fddffd
egrf <egrf.dev@gmail.com>
2022-07-04 18:16:13 +0300

    ui added

--- app/src/main/java/com/egrf/contactsapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/egrf/contactsapp/ui/main/MainFragment.kt
@@ -1,12 +1,11 @@
 package com.egrf.contactsapp.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.egrf.contactsapp.R
+import com.egrf.contactsapp.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
@@ -15,18 +14,25 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
 }
